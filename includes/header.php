0cfc19af2bbef2640f8b715953e562c454fda513
Lili Mkrtchyan <lm688@cornell.edu>
2023-02-16 00:10:20 +0000

    Included the transportation page.

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
       <li class="<?php echo $home; ?>"><a href="index.php">Home</a></li>
       <li class="<?php echo $vendors; ?>"><a href="vendor.php">Vendors</a></li>
       <li class="<?php echo $entertainment; ?>"><a href="entertainment.php">Entertainment</a></li>
-      <!-- <li class="<?php echo $transportation; ?>"><a href="transportation.php">Transportation</a></li> -->
+      <li class="<?php echo $transportation; ?>"><a href="transportation.php">Transportation</a></li>
       <li class="<?php echo $faq; ?>"><a href="faq.php">FAQ</a></li>
     </ul>
   </nav>
